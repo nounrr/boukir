b23792dc591218202a702b98f7d7886689e452ed
nounrr <sosi66581@gmail.com>
2025-08-14 23:31:47 +0100

    first commit

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,11 +1,23 @@
 import 'dotenv/config';
 import mysql from 'mysql2/promise';
+// local
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST || 'localhost',
+//   port: Number(process.env.DB_PORT || 3307),
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASSWORD || 'rootroot@',
+//   database: process.env.DB_NAME || 'boukir',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+// prod
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT || 3307),
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'rootroot@',
+  port: Number(process.env.DB_PORT || 3306),
+  user: process.env.DB_USER || 'boukir',
+  password: process.env.DB_PASSWORD || 'Ton46-l,yk,hbMotDePasse',
   database: process.env.DB_NAME || 'boukir',
   waitForConnections: true,
   connectionLimit: 10,
