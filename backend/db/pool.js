60549a946c4be0c01e4105cd8da74636b61804db
Med Nour <67807928+nounrr@users.noreply.github.com>
2025-09-22 17:39:49 +0100

    pool

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -8,16 +8,11 @@ export const requestContext = new AsyncLocalStorage();
 // -----------------------------------------------------------------------------
 // POOL LOCAL (commenté)
 // -----------------------------------------------------------------------------
-
-
-// -----------------------------------------------------------------------------
-// POOL PROD (ACTIF) — créé UNE fois ici (pas dans getConnection)
-// -----------------------------------------------------------------------------
 // const pool = mysql.createPool({
 //   host: process.env.DB_HOST || 'localhost',
-//   port: Number(process.env.DB_PORT || 3306),
-//   user: process.env.DB_USER || 'boukir',
-//   password: process.env.DB_PASSWORD || 'Ton46-l,yk,hbMotDePasse',
+//   port: Number(process.env.DB_PORT || 3307),
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASSWORD || 'rootroot@',
 //   database: process.env.DB_NAME || 'boukir',
 //   waitForConnections: true,
 //   connectionLimit: 10,
@@ -25,9 +20,23 @@ export const requestContext = new AsyncLocalStorage();
 // });
 
 // -----------------------------------------------------------------------------
+// POOL PROD (ACTIF) — créé UNE fois ici (pas dans getConnection)
+// -----------------------------------------------------------------------------
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT || 3306),
+  user: process.env.DB_USER || 'boukir',
+  password: process.env.DB_PASSWORD || 'Ton46-l,yk,hbMotDePasse',
+  database: process.env.DB_NAME || 'boukir',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+// -----------------------------------------------------------------------------
 // Patch getConnection : injecter @app_user_id / @app_request_id si disponibles
 // -----------------------------------------------------------------------------
-//const originalGetConnection = pool.getConnection.bind(pool);
+const originalGetConnection = pool.getConnection.bind(pool);
 pool.getConnection = async function patchedGetConnection() {
   const conn = await originalGetConnection();
   try {
